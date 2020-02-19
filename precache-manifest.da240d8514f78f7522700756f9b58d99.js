9db64a8ca342ef5c5c0fcbb9db7fc30ddfc2f558
root <root@DESKTOP-QS49P76.localdomain>
2020-02-19 14:01:00 -0500

    Updates

---
+++ precache-manifest.da240d8514f78f7522700756f9b58d99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2670d0a2265657a4dec35f4b1a7c693c",
+    "url": "mato9020.github.io/index.html"
+  },
+  {
+    "revision": "53c95ac6c1cd7a2cf945",
+    "url": "mato9020.github.io/static/css/main.2eb0803a.chunk.css"
+  },
+  {
+    "revision": "59102ade064ffb0abf4b",
+    "url": "mato9020.github.io/static/js/2.261f5561.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "mato9020.github.io/static/js/2.261f5561.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53c95ac6c1cd7a2cf945",
+    "url": "mato9020.github.io/static/js/main.3da59213.chunk.js"
+  },
+  {
+    "revision": "79e4c8a1cac9c69fc6a5",
+    "url": "mato9020.github.io/static/js/runtime-main.bdbb0cef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "mato9020.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b2c824afd142c62b7d55e1ce38f9001d",
+    "url": "mato9020.github.io/static/media/profile.b2c824af.jpg"
+  }
+]);
